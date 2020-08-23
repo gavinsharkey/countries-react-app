84cdb81c5c46d6fbc4a186405c2bc604727e6272
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-23 16:23:15 -0400

    Updates

---
+++ precache-manifest.3fb8d9e5c2af1c4f704db959123d124d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e91161aa9c34cd9f7a17bc8f481ceaa",
+    "url": "/countries-react-app/index.html"
+  },
+  {
+    "revision": "3f7b4c0f5e68925f2742",
+    "url": "/countries-react-app/static/css/main.898078c1.chunk.css"
+  },
+  {
+    "revision": "80e7aabc33916f89d09e",
+    "url": "/countries-react-app/static/js/2.602b41e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countries-react-app/static/js/2.602b41e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7b4c0f5e68925f2742",
+    "url": "/countries-react-app/static/js/main.eebfed45.chunk.js"
+  },
+  {
+    "revision": "88e1e4e0052423019c24",
+    "url": "/countries-react-app/static/js/runtime-main.8d60ed0c.js"
+  }
+]);
